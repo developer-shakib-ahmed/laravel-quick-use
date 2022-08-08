c58ea2e5f6c0aa87da0badc617ea748eac65a7aa
SonyPc <wpbestcoder@gmail.com>
2022-08-08 10:44:11 +0600

    Update File

--- laravel-blade-help.php
+++ laravel-blade-help.php
@@ -13,3 +13,20 @@
 @else
     
 @endauth
+
+
+
+<!-- Laravel Logout Route -->
+@auth
+  <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+  <li>
+    <form method="POST" action="{{ route('logout') }}">
+      @csrf
+      <a href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
+          {{ __('Log Out') }}
+      </a>
+    </form>
+  </li>
+@else
+  <li><a href="{{ route('login') }}">Login</a></li>
+@endauth
